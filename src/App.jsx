596a322d438006bc06b1145fc4f124069c7af20c
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-02-15 15:46:16 -0300

    Se agrega música y animación del fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import s from "./style.module.css"
 import Characters from "./components/Character";
+import { useState, useEffect } from "react";
+import openingTheme from "./assets/audio/openingTheme.mp3"
 
 function App() {
+
+  const [audio] = useState(new Audio(openingTheme)); // Inicializa el audio usando el archivo mp3
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    playing ? audio.loop = true && audio.play() : audio.pause(); // Actualiza el estado de reproducción del audio en base al estado 'playing'
+  }, [playing, audio]);
+
   return (
     <div>
+      <button onClick={() => setPlaying(!playing)}> {/* Cambia el estado 'playing' al hacer clic en el botón */}
+        {playing ? "Pause" : "Play"}
+      </button>
+
+      
       < Characters/>
     </div>
   );
